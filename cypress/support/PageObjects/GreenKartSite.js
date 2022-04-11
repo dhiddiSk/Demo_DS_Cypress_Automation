202c0c1e407628dde649bd5c8fe91b97d893700f
saikrishna <saikrishna.4369@gmail.com>
2022-04-11 20:26:41 +0200

    restructuring and eleminating unecessary files

--- cypress/support/PageObjects/GreenKartSite.js
+++ cypress/support/PageObjects/GreenKartSite.js
@@ -1,35 +1,26 @@
-class GreenKartsite{
-
-AddvegetablesToCartBysearchKeyword(keyword, ...keywordMatchingVegetablesList){
-    
-    cy.visit(Cypress.env('vegetablesHomepageUrl'));
-
-    cy.get(".search-keyword").type(keyword);
-    
-    cy.wait(3000);
-
-    cy.get('.products').find('.product').each(($el, index, $list) => {
-    
-        if(keywordMatchingVegetablesList.includes($el.find('h4.product-name').text())){
-    
+class GreenKartsite {
+    AddvegetablesToCartBysearchKeyword(
+      keyword,
+      ...keywordMatchingVegetablesList
+    ) {
+      cy.get(".search-keyword").type(keyword);
+  
+      cy.get(".products")
+        .find(".product")
+        .each(($el, index, $list) => {
+          if (
+            keywordMatchingVegetablesList.includes(
+              $el.find("h4.product-name").text()
+            )
+          ) {
             $el.find("button[type=button]").click();
-    
-        
           }
-
-    })
-
-
+        });
     }
-
-numberOfItemsIncart(){
-
-    return Numeber(cy.get('.cart-count').text());
-
-
-
-
-}
-
-}    
-export default GreenKartsite;
+  
+    numberOfItemsIncart() {
+      return Number(cy.get(".cart-count").text());
+    }
+  }
+  export default GreenKartsite;
+  
