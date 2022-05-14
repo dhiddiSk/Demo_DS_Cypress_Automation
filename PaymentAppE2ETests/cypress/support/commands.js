4adbb77312bf958657e3a93ce6c7c2bea72f2d48
saikrishna <saikrishna.4369@gmail.com>
2022-05-14 12:09:23 +0200

    added cypress login command

--- PaymentAppE2ETests/cypress/support/commands.js
+++ PaymentAppE2ETests/cypress/support/commands.js
@@ -10,7 +10,31 @@
 //
 //
 // -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
+Cypress.Commands.add("login", (email, password) => {
+  if (loginEmailAddress === undefined && loginPassword === undefined) {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("loginApi_url"),
+      body: {
+        email: constant.userCredentials.userName,
+        password: constant.userCredentials.userPassowrd,
+      },
+    }).then((resp) => {
+      window.localStorage.setItem("token", resp.body.token);
+    });
+  } else {
+    cy.request({
+      method: "POST",
+      url: Cypress.env("loginApi_url"),
+      body: {
+        email: email,
+        password: password,
+      },
+    }).then((resp) => {
+      window.localStorage.setItem("token", resp.body.token);
+    });
+  }
+});
 //
 //
 // -- This is a child command --
